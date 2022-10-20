43474552c47168a3fcfcfbccc5d9c9089b056311
Joao Garduño <joao.garduno@gmail.com>
2022-10-19 23:11:17 -0500

    Estructura 2: Leyendo HTML desde Javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log('Hello, World');
+// creamos una variale
+const h1 = document.querySelector('h1');
+
+const p = document.querySelector('p');
+
+const parrafo = document.querySelector('.paragraph');
+
+const pid = document.querySelector('#paragraphId');
+
+const input = document.querySelector('input');
+
+// Haremos un console.log solo de un elemento para identificar que es lo que esta sucediendo
+console.log(h1);
+
+// c
+console.log(input.value);
+
+//  Objeto de nuestros elementos para ver sus propiedades o funciones en el navegador
+console.log({
+    h1,
+    parrafo,
+    pid,
+    input,
+});
+
+// ejemplo con "input"
+// function myInputText(){
+//     const inputText = document.getElementById('input-id').style.backgroundColor = 'blue';
+// }
