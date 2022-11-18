7f12b974e43b72465157c0aa164c32bdc5e8d1c9
Joao Garduño <joao.garduno@gmail.com>
2022-11-17 20:05:58 -0600

    Estructura 4: Eventos en JavaScript - Interactuando con el Usuario

--- js/script.js
+++ js/script.js
@@ -1,69 +1,27 @@
-// creamos una variale
-const h1 = document.querySelector('h1');
-const h2 = document.querySelector('h2');
-const h3 = document.querySelector('h3');
-const p = document.querySelector('p');
-const parrafo = document.querySelector('.paragraph');
-const pid = document.querySelector('#paragraphId');
-const input = document.querySelector('input');
+// variables
+const titleH1 = document.querySelector('#titlePage');
+const input1 = document.querySelector('#calculo1');
+const input2 = document.querySelector('#calculo2');
+const btn = document.querySelector('#btnCalcular');
 
-// Haremos un console.log solo de un elemento para identificar que es lo que esta sucediendo
-console.log(h1);
-
-// c
-console.log(input.value);
-
-//  Objeto de nuestros elementos para ver sus propiedades o funciones en el navegador
-console.log({
-    h1,
-    parrafo,
-    pid,
-    input,
-});
-
-// MODIFICAR ELEMENTOS DE TIPO TEXTO Y ELEMENTOS DE TIPO HTML CON ".innerHTML" e ".innerText"
-// h1.innerHTML = 'Seguimos aprendiendo para mejorar la manipulación del DOM';
-// h1.innerHTML = 'Este es un nuevo ejemplo <br> con un salto de linea utilizando etiqueta "br"';
-// h1.innerText = 'Este es un ejemplo con texto, sin interpretar elementos HTML <br>';
-
-// OBTENER O SELECCIONAR ATRIBUTOS CON ".getAttribute" y ".setAttribute"
-// console.log(h2.getAttribute('pantalla'));
-// console.log(h2.getAttribute('class'));
-console.log(h3.getAttribute('class'));
-
-// MODIFICAR ESE ATRIBUTO DE TIPO CLASE O CLASS
-// h3.setAttribute('class', 'blue');
-
-// AGREGAR ATRIBUTOS DE CLASS
-h3.classList.add('purple');
-h3.classList.remove('green');
-
-// EVENTOS
-h3.classList.toggle('red');
-
-// TIPO DE CONDICIONAL TRUE O FALSE
-h3.classList.contains('purple');
-
-// ENTRAR AL VALOR DEL INPUT
-input.value = "456";
+// RESULTADO
+const pResult = document.querySelector('#result');
 
 
-// CREAR ELEMENTO HTML DE 0 DESDE JAVASCRIPT UTILIZANDO:
-// document.createElement('');
+// Funcion del botón
+function btnOnClick(){
+    // console.log('Escuchando el evento click desde una funcion');
 
-// SI CREAMOS UNA IMAGEN LO HACEMOS ASI
-// console.log(document.createElement('img'));
-// console.log(document.createElement('span'));
+    // De esta manera solo esta "concatenando los valores" necesitamos indicarle que son numeros
+    // console.log(input1.value + input2.value);
 
-// lo primero que hacemos es crear nuestra imagen en JavaScript guardandola en una variable llamada imagen
-const imagen = document.createElement('img');
-imagen.setAttribute('src', 'https://images.unsplash.com/photo-1666899155015-d33935023d20?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80');
+    // console.log(parseInt(input1.value) + parseInt(input2.value));
 
-// imprimimos nuestra variable "imagen"
-console.log('imagen');
+    // RESOLVIENDO LA OPERACION Y MOSTRANDO RESULTADO EN PANTALLA
+    const sumaOperation = parseInt(input1.value) + parseInt(input2.value);
 
-// pid.append(imagen);
-
-// RETO: Borrando el contenido del parrafo y solo agregando la imagen dentro de este parrafo
-pid.innerHTML = '';
-pid.appendChild(imagen);
+    // pResult.innerText = 'El resultado de esta SUMA es: ' + sumaOperation;
+    
+    // OTRA FORMA 
+    pResult.innerText = `El resultado de esta SUMA es: ${sumaOperation}`;
+}
